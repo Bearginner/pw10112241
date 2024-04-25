3a32943045cd88b09cc3978f6b3f6f4cc8f26f34
Victor Manuel Valdes Machado <146697122+Bearginner@users.noreply.github.com>
2024-04-25 10:51:12 -0700

    Backend - Conexion a MySQL

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,6 +6,35 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexion a MYSQL
+let conexion = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pwdata'
+});
+
+//Nos conectamos a MYSQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    } else {
+        console.log('Conectado a la base de datos');
+    }
+});
+
+//Seleccionamos todos los clientes
+app.get('/api/clientes', (req, res) =>{
+    conexion.query('Select * From clientes', (error, filas)=>{
+        if(error){
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    });
+}
+);
+
 // Rutas de Acceso
 app.get("/", function(req, res){
     res.send("Emilio, si estas viendo holis");
