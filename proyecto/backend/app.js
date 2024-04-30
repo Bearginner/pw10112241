378a7a13d7a8abf3557ec44761367ff1de0165d0
Victor Manuel Valdes Machado <146697122+Bearginner@users.noreply.github.com>
2024-04-30 10:56:44 -0700

    Backend - Borrar e Insertar datos

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 //Conexion a MYSQL
 let conexion = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: '',
     database: 'pwdata'
@@ -40,8 +40,52 @@ app.get("/", function(req, res){
     res.send("Emilio, si estas viendo holis");
 })
 
+//Seleccionamos un cliente en especifico
+app.get('/api/clientes/:id', (req, res) =>{
+    conexion.query('Select * From clientes Where id = ?', [req.params.id], (error, fila) =>{
+        if(error){
+            throw error;
+        } else {
+            res.send(fila);
+        }
+    })
+});
+
+//Eliminamos cliente
+app.delete('/api/clientes/:id', (req, res) => {
+    let id = req.params.id;
+    conexion.query('Delete From clientes Where id = ?', [id], (error, filas) =>{
+        if(error){
+            throw error;
+        } else {
+            res.send(filas);
+        }
+    })
+});
+
+//Insertar cliente
+app.post('/api/clientes', (req, res) => {
+    let data = {
+        id:req.body.id,
+        nombre: req.body.nombre,
+        direccion: req.body.direccion,
+        telefono: req.body.telefono,
+        rfc: req.body.rfc
+    }
+    let sql = "Insert Into clientes Set ?";
+    conexion.query(sql, data, (error, resultado) =>{
+        if(error){
+            throw error;
+        } else {
+            res.send(resultado);
+        }
+    })
+});
+
 // Encender el server
 let puerto = 3000;
 app.listen(puerto, function(){
     console.log("Servidor escuchando puerto "+puerto);
 });
+
+//Eliminar las APIS;  rm -rf node_modules <- Solo MAC
